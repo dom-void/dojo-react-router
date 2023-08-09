5c9ab46d82f089cb615322e20117048e835479b8
dom void <doe@onet.pl>
2023-08-09 13:05:03 +0200

    Add url params in loaders

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,9 @@ import Root, {
   action as rootAction,
 } from './routes/root'
 import ErrorPage from './error-page'
-import Contact from './routes/contact'
+import Contact, {
+  loader as contactLoader,
+} from './routes/contact'
 import './index.css'
 
 const router = createBrowserRouter([
@@ -23,6 +25,7 @@ const router = createBrowserRouter([
       {
         path: 'contacts/:contactId',
         element: <Contact />,
+        loader: contactLoader,
       }
     ],
   },
