6ebcb76c39f3a63c1efb40924f20c19b36161053
dom void <doe@onet.pl>
2023-08-09 12:58:19 +0200

    Add contacts creating

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,10 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from 'react-router-dom'
-import Root, { loader as rootLoader } from './routes/root'
+import Root, { 
+  loader as rootLoader,
+  action as rootAction,
+} from './routes/root'
 import ErrorPage from './error-page'
 import Contact from './routes/contact'
 import './index.css'
@@ -15,6 +18,7 @@ const router = createBrowserRouter([
     element: <Root />,
     errorElement: <ErrorPage />,
     loader: rootLoader,
+    action: rootAction,
     children: [
       {
         path: 'contacts/:contactId',
